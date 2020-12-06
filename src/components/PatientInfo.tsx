35204d1a048bbec01180c63d641bfbc931e667a2
Michael <Mick666@hotmail.com.au>
2020-12-07 04:32:16 +1100

    Added strict type checking

--- src/components/PatientInfo.tsx
+++ src/components/PatientInfo.tsx
@@ -82,6 +82,11 @@ const OccupationalHealthcareTable: React.FC<{ entry: OccupationalHealthcareEntry
   );
 };
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled type: ${JSON.stringify(value)}`
+  );
+};
 
 const EntryDetails = (entry: Entry) => {
   switch (entry.type) {
@@ -92,7 +97,7 @@ const EntryDetails = (entry: Entry) => {
     case "OccupationalHealthcare":
       return <OccupationalHealthcareTable entry={entry} />;
     default:
-      return null;
+      return assertNever(entry);
   }
 };
 
